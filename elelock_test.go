d781c0bc3476bb9fa47e9b7fb273246e3d504fee
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2022-01-14 03:43:20 +0900

    add test

--- elelock_test.go
+++ elelock_test.go
@@ -6,21 +6,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestElelock01(t *testing.T) {
-	elelock := NewElelock()
+// func TestElelock01(t *testing.T) {
+// 	elelock := NewElelock()
 
-	elelock.key.Set(1)
-	elelock.Exec()
-	assert.Equal(t, 0, elelock.lock.ToInt())
-}
+// 	elelock.key.Set(1)
+// 	elelock.Exec()
+// 	assert.Equal(t, 0, elelock.lock.ToInt())
+// }
 
-func TestElelock02(t *testing.T) {
-	elelock := NewElelock()
+// func TestElelock02(t *testing.T) {
+// 	elelock := NewElelock()
 
-	elelock.close.Set(1)
-	elelock.Exec()
-	assert.Equal(t, 1, elelock.lock.ToInt())
-}
+// 	elelock.close.Set(1)
+// 	elelock.Exec()
+// 	assert.Equal(t, 1, elelock.lock.ToInt())
+// }
 
 func TestElelock03(t *testing.T) {
 	elelock := NewElelock()
@@ -29,4 +29,33 @@ func TestElelock03(t *testing.T) {
 	elelock.key.Set(1)
 	elelock.clk.Set(1)
 	assert.Equal(t, 0, elelock.lock.ToInt())
+}
+
+func TestElelock04(t *testing.T) {
+	elelock := NewElelock()
+
+	elelock.clk.Set(0)
+	elelock.close.Set(1)
+	elelock.clk.Set(1)
+	assert.Equal(t, 1, elelock.lock.ToInt())
+}
+
+func TestElelock05(t *testing.T) {
+	elelock := NewElelock()
+
+	elelock.clk.Set(0)
+	elelock.close.Set(1)
+	elelock.clk.Set(1)
+	assert.Equal(t, 1, elelock.lock.ToInt())
+	elelock.clk.Set(0)
+	elelock.close.Set(0)
+	elelock.key.Set(1)
+	elelock.clk.Set(1)
+	assert.Equal(t, 0, elelock.lock.ToInt())
+	elelock.clk.Set(0)
+	elelock.key.Set(0)
+	elelock.close.Set(1)
+	elelock.clk.Set(1)
+	assert.Equal(t, 1, elelock.lock.ToInt())
+	elelock.clk.Set(0)
 }
