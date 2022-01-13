25cbc8ad66619f5bae443c0ef3eb4faa25c81d79
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2022-01-13 17:36:53 +0900

    red

---
+++ elelock.go
@@ -0,0 +1,37 @@
+package main
+
+import "github.com/Verilog2go-TDD-example/src/variable"
+
+type Elelock struct {
+	key, lock *variable.BitArray
+}
+
+func NewElelock() Elelock {
+	args := &Elelock{variable.NewBitArray(1), variable.NewBitArray(1)}
+	return *args
+}
+
+func NewGoroutineElelock(in []chan int, out []chan int) *Elelock {
+	elelock := &Elelock{variable.NewBitArray(1), variable.NewBitArray(1)}
+	go elelock.start(in, out)
+	return elelock
+}
+
+func (elelock *Elelock) Exec() {
+}
+
+func (elelock *Elelock) start(in []chan int, out []chan int) {
+	defer close(out[0])
+	for {
+		select {
+		case v, ok := <-in[0]:
+			if ok {
+				elelock.key.Set(v)
+				elelock.Exec()
+				out[0] <- elelock.lock.ToInt()
+			} else {
+				return
+			}
+		}
+	}
+}
---
+++ elelock.v
@@ -0,0 +1,4 @@
+module elelock(key, lock);
+    input key;
+    output lock;
+endmodule
